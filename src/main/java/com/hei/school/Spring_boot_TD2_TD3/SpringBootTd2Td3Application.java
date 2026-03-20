58deef7fee6bfc2c9b812b48a2b65c6c81581aa8
Lavorary <hei.lavorary@gmail.com>
2026-03-20 15:20:02 +0300

    inital commit

---
+++ src/main/java/com/hei/school/Spring_boot_TD2_TD3/SpringBootTd2Td3Application.java
@@ -0,0 +1,13 @@
+package com.hei.school.Spring_boot_TD2_TD3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTd2Td3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTd2Td3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hei/school/Spring_boot_TD2_TD3/SpringBootTd2Td3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.school.Spring_boot_TD2_TD3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTd2Td3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
